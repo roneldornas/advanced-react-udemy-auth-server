fb3e48abafdf0da4dda8c06b3d5482a23b87b177
Ronel Dornas <roneldornas@mail.com>
2019-03-05 22:25:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//Main start point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:27017/auth', { 
+    useNewUrlParser: true,
+    useCreateIndex: true
+ });
+
+// App setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({type: '*/*'}));
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log(`Server listening on: ${port}`);
